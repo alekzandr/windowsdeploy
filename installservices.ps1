beac7f65a0ec67e3e0159ccd6d16078c40358f9c
Kyle Topasna <kyle.topasna@gmail.com>
2021-07-13 13:43:44 -0800

    Uploaded sysmon, install script, and instructions

---
+++ installservices.ps1
@@ -0,0 +1,15 @@
+﻿cd $env:USERPROFILE\Desktop\windowsdeploy
+
+Copy-Item -Force -Recurse -Verbose .\sysmon -Destination 'C:\Program Files'
+
+Copy-Item -Force -Recurse -Verbose .\winlogbeat -Destination 'C:\Program Files'
+
+cd -Path 'C:\Program Files\sysmon'
+
+.\sysmon64.exe -accepteula -i 'C:\Program Files\sysmon\sysmonconfig-export.xml'
+
+cd 'C:\Program Files\winlogbeat'
+
+.\install-service-winlogbeat.ps1
+
+Start-Service winlogbeat
