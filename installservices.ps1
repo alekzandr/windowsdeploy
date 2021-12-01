a2163f9fb7c2e70a600cd03614f5995412503843
Kyle Topasna <kyle.topasna@gmail.com>
2021-11-30 22:12:54 -0900

    Update installservices.ps1

--- installservices.ps1
+++ installservices.ps1
@@ -1,15 +1,26 @@
-﻿cd $env:USERPROFILE\Desktop\windowsdeploy
+#Download winlogbeats from Elastic and uncompress it.
+$ProgressPreference = 'SilentlyContinue'
+wget https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.15.2-windows-x86_64.zip -UseBasicParsing -OutFile $env:USERPROFILE\Downloads\winlogbeat-7.15.2-windows-x86_64.zip
+Expand-Archive -Path $env:USERPROFILE\Downloads\winlogbeat-7.15.2-windows-x86_64.zip -DestinationPath $env:USERPROFILE\Downloads\winlogbeat-7.15.2-windows-x86_64
 
+# Copy the Elastic Winlogbeat executable into our file location
+# then move sysmon and Winlogbeat into C:\Program Files
+Copy-Item -Force -Verbose $env:USERPROFILE\Downloads\winlogbeat-7.15.2-windows-x86_64\winlogbeat-7.15.2-windows-x86_64\winlogbeat.exe -Destination .\winlogbeat\winlogbeat.exe
 Copy-Item -Force -Recurse -Verbose .\sysmon -Destination 'C:\Program Files'
-
 Copy-Item -Force -Recurse -Verbose .\winlogbeat -Destination 'C:\Program Files'
 
-cd -Path 'C:\Program Files\sysmon'
+# Run and install sysmon
+& "C:\Program Files\sysmon\sysmon64.exe" -accepteula -i 'C:\Program Files\sysmon\sysmonconfig-export.xml'
 
-.\sysmon64.exe -accepteula -i 'C:\Program Files\sysmon\sysmonconfig-export.xml'
+# Install winlogbeat service
+& "C:\Program Files\winlogbeat\install-service-winlogbeat.ps1"
 
-cd 'C:\Program Files\winlogbeat'
+#Wait for install-service-winlogbeat.ps1 to finish
+start-sleep -Seconds 5
 
-.\install-service-winlogbeat.ps1
-
+#start Winlogbeat
 Start-Service winlogbeat
+
+#Print Statuses
+get-service winlogbeat
+get-service sysmon64
