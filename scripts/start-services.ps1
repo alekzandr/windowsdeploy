d8ddee9d04eb6aa8ea932fdd0894c5ea731c1d06
Kyle Topasna <kyle.topasna@gmail.com>
2022-05-25 14:25:24 -0800

    Update start-services.ps1

--- scripts/start-services.ps1
+++ scripts/start-services.ps1
@@ -4,7 +4,7 @@ $computers = gc computers.txt
 $script_block = {
     param ($computer_name, $online)
     if ($online) {
-        Invoke-Command -ComputerName $computer_name -ScriptBlock {copy-item -Force "\\172.17.40.4\c$\windowsdeploy-main\windowsdeploy-main\winlogbeat\winlogbeat.yml" -Destination "C:\Program Files\winlogbeat\"
+        Invoke-Command -ComputerName $computer_name -ScriptBlock {copy-item -Force "\\172.17.40.4\c$\windowsdeploy-main\windowsdeploy-main\winlogbeat\winlogbeat.yml" -Destination "C:\Program Files\winlogbeat\"}
         Invoke-Command -ComputerName $computer_name -ScriptBlock {sc.exe config winlogbeat start= auto; sc.exe config sysmon64 start= auto; start-Service winlogbeat; start-service sysmon64}
     }
 }
