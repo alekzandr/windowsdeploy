e0954ef465b26137d18406efb5ba4cd3729b0215
Kyle Topasna <kyle.topasna@gmail.com>
2022-05-27 17:49:00 -0800

    Update start-services.ps1

--- scripts/start-services.ps1
+++ scripts/start-services.ps1
@@ -4,14 +4,7 @@ $computers = gc computers.txt
 $script_block = {
     param ($computer_name, $online)
     if ($online) {
-        Invoke-Command -ComputerName $computer_name -ScriptBlock {
-        $ACL = Get-ACL -Path "C:\Program Files\winlogbeat\";
-        $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("basex\cobra","FullControl","Allow");
-        $ACL.SetAccessRule($AccessRule);
-        $ACL | Set-Acl -Path "C:\Program Files\winlogbeat\";
-        (Get-ACL -Path "C:\Program Files\winlogbeat\").Access | Format-Table IdentityReference,FileSystemRights,AccessControlType,IsInherited,InheritanceFlags -AutoSize;
-        }
-        Invoke-Command -ComputerName $computer_name -ScriptBlock {copy-item -Force "\\172.17.40.4\c$\windowsdeploy-main\windowsdeploy-main\winlogbeat\winlogbeat.yml" -Destination "C:\Program Files\winlogbeat\"}
+        copy-item -Force "C:\windowsdeploy-main\windowsdeploy-main\winlogbeat\winlogbeat.yml" -Destination "\\phnlclient67\c$\Program Files\winlogbeat"
         Invoke-Command -ComputerName $computer_name -ScriptBlock {sc.exe config winlogbeat start= auto; sc.exe config sysmon64 start= auto; start-Service winlogbeat; start-service sysmon64}
     }
 }
