cbf832aca52468d4a1b50012c713b9116037d122
Kyle Topasna <kyle.topasna@gmail.com>
2022-05-27 18:25:52 -0800

    Fixed error with copying to only one machine

--- scripts/start-services.ps1
+++ scripts/start-services.ps1
@@ -4,7 +4,7 @@ $computers = gc computers.txt
 $script_block = {
     param ($computer_name, $online)
     if ($online) {
-        copy-item -Force "C:\windowsdeploy-main\windowsdeploy-main\winlogbeat\winlogbeat.yml" -Destination "\\phnlclient67\c$\Program Files\winlogbeat"
+        copy-item -Force "C:\windowsdeploy-main\windowsdeploy-main\winlogbeat\winlogbeat.yml" -Destination "\\$computer_name\c$\Program Files\winlogbeat"
         Invoke-Command -ComputerName $computer_name -ScriptBlock {sc.exe config winlogbeat start= auto; sc.exe config sysmon64 start= auto; start-Service winlogbeat; start-service sysmon64}
     }
 }
